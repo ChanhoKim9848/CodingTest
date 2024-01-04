365b1b11422860c5d24f1434ad2e13dc741a06e5
detol3953 <chanhokim9848@gmail.com>
2024-01-04 14:29:05 +0900

    2024-01-04

--- assign-cookies.py
+++ assign-cookies.py
@@ -1,22 +1,18 @@
-class Solution(object):
-    def findContentChildren(self, g, s):
-        """
-        :type g: List[int]
-        :type s: List[int]
-        :rtype: int
-        """
-        g.sort()
-        s.sort()
+def findContentChildren(self, g: List[int], s: List[int]) -> int:
+    
+    if not len(s):
+        return 0
+    s.sort()
+    g.sort()
+    result=0
+    i,j=0,0
+    
+    while i<len(g) and j<len(s):
         
-        childi = 0
-        cookiei = 0
-        
-        while cookiei < len(s) and childi < len(g):
-            if s[cookiei] >= g[childi]:
-                childi += 1
-            cookiei += 1
-        
-        return childi
-
-
-        # O(nlogn)
+        if g[i]<=s[j]:
+            i+=1
+            j+=1
+            result+=1
+        else:
+            j+=1
+    return result
